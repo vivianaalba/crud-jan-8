c5fe98015f8e8b01fdb27c19e71aa3ef49c9bee8
Viviana Alba <vivianaalba7@gmail.com>
2024-01-08 19:51:41 -0800

    configure server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
 
 app.listen(PORT, function() {
     console.log(`Server is live! Listening at port ${PORT}`); })
