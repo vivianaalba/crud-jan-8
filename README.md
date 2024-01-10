1eb0489a6de485082f2e34d81ac5c086473f37ed
Viviana Alba <vivianaalba7@gmail.com>
2024-01-09 19:28:35 -0800

    update create assignment

--- README.md
+++ README.md
@@ -7,9 +7,10 @@
 
 ### Getting started
 <br/>How does your app currently function and what can the user do with it?
-<br/>Website is currently connected to a local server and has a dev dependency installed for testing purposes
+<br/>Website is currently connected to a local server and has a dev dependency installed for testing purposes. We also installed a middleware called body-parser so that the server can communicate with the client side.
 
 #### Installation
 <br/>npm init
 <br/>npm install express --save
 <br/>npm install nodemon --save-dev
+<br/>npm install body-parser --save
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// test to confirm that server is running
 console.log('Is this thing on?');
 
-
+// include express and body-parser modules in code
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -10,13 +11,18 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
+// when a webpage has an address that includes '/' we are making a GET request
+// requesting static or dynamic files stored in that address
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
+// returns credentials to console
+// allows the client side to communicate with server
 app.post('/users', (req, res) => {
     console.log(req.body);
 })
 
+// stays at the bottom
 app.listen(PORT, function() {
     console.log(`Server is live! Listening at port ${PORT}`); })
