ef4c20ba66fce1be57b81a64940325fa7dca1f91
Viviana Alba <vivianaalba7@gmail.com>
2024-01-09 19:03:08 -0800

    install middleware

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ console.log('Is this thing on?');
 
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+// remains above CRUD code
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/users', (req, res) => {
+    console.log(req.body);
 })
 
 app.listen(PORT, function() {
